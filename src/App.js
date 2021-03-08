0b189e9fc3afc47d02cae8cf67835b86df6b3539
Shaoors <shaoors16.github.com>
2021-03-08 16:28:36 +0500

    made homepage with components and styled it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homapage/homapage.component";
 function App() {
   return (
     <div>
