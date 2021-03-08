a2e1eef3f3c907a6275dd959100614cd7df1ccb1
Shaoors <shaoors16.github.com>
2021-03-08 21:27:35 +0500

    Made just one router for hat page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homapage/homapage.component";
+
+const Hatspage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={Hatspage} />
+      </Switch>
     </div>
   );
 }
