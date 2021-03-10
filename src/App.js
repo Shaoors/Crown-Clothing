e89e409fa5696b52b0c4d0fd6f9c00ae9f819ba0
Shaoors <shaoors16.github.com>
2021-03-10 19:54:54 +0500

    Shop page collection Preview and collection Items ceated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homapage/homapage.component";
-
+import ShopPage from "./pages/homapage/shop/shop.component";
 const Hatspage = () => (
   <div>
     <h1>HATS PAGE</h1>
@@ -13,7 +13,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={Hatspage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
