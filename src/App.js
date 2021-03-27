3fa6ccc0270f69d08ec341425e148884f9d43f3b
shaoors <shaoors16@outlook.com>
2021-03-27 16:20:37 +0500

    made Signup and stored in firestore

--- src/App.js
+++ src/App.js
@@ -22,8 +22,22 @@ class App extends React.Component {
 
   unsubscribeFromAuth= null;
   componentDidMount(){
-    auth.onAuthStateChanged(async user =>{
-      createUserProfileDocument(user);
+    auth.onAuthStateChanged(async userAuth =>{
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+
+        userRef.onSnapshot(snapShot=>{
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          }) 
+        });
+        
+      }
+      this.setState({currentUser: userAuth});
     })
   }
 
