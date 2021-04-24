0ef4c8f533d8b099ea1175cdedef742dab7e1e2c
shaoors <shaoors16@outlook.com>
2021-04-24 16:14:03 +0500

    Added cart items with reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,4 +2,9 @@ import CartActionTypes from "./cart.types";
 //ismay q () ye
 export const toggleCartHidden =()=>({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
+});
+
+export const addItem= item =>({
+    type:CartActionTypes.ADD_ITEM,
+    payload: item
+});
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 //ismay q ni () ye
 const CartActionTypes ={
-    TOGGLE_CART_HIDDEN:'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN:'TOGGLE_CART_HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
 };
 export default CartActionTypes;
 
